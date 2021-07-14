389d26498fe38ee444259bfd348748cf971404c3
sashasabater <sasha.sabater@gmail.com>
2021-07-14 13:10:56 -0400

    testing models

--- src/helper.py
+++ src/helper.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
+from sklearn.metrics import mean_squared_error as mse 
+
 
 
 
@@ -12,6 +14,12 @@ def plot_cols(df,num, target):
         ax.scatter(df[col], df[target])
         ax.set_title(f'{col}')
         ax.set_ylabel(f'{target}')
+
+        
+def model_test(model, X_tn, X_tst, y_tn, y_tst):
+    model = model.fit(X_tn, y_tn)
+    yhat = model.predict(X_tst)
+    return mse(y_tst, yhat) ** .5
         
     
     
